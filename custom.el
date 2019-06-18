a1cef2740fa42f4e862738829d326c7e958cb50d
Hajime ARAI <hajime.arai@gmail.com>
2019-06-18 15:54:06 +0900

    confの内容をinit.elに一本化

--- custom.el
+++ custom.el
@@ -9,9 +9,14 @@
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "713f898dd8c881c139b62cf05b7ac476d05735825d49006255c0a31f9a4f46ab" "4e63466756c7dbd78b49ce86f5f0954b92bf70b30c01c494b37c586639fa3f6f" default)))
  '(package-selected-packages
    (quote
-    (ini-mode use-package git-gutter helm-gtags yaml-mode php-mode web-mode shackle undo-tree helm-descbinds sticky flycheck-pos-tip helm-elscreen elscreen ssh-agency helm-projectile projectile magit ddskk json-mode rotate howm undohist recentf-ext volatile-highlights tangotango-theme company-jedi company-quickhelp company markdown-mode open-junk-file anzu helm-swoop js2-mode fill-column-indicator quickrun py-yapf flycheck init-loader which-key bind-key smartparens rainbow-delimiters multiple-cursors migemo hungry-delete helm-c-yasnippet helm-ag expand-region)))
+    (ini-mode use-package git-gutter helm-gtags yaml-mode php-mode web-mode shackle undo-tree helm-descbinds sticky flycheck-pos-tip helm-elscreen elscreen ssh-agency helm-projectile projectile magit ddskk json-mode rotate howm undohist recentf-ext volatile-highlights tangotango-theme company-jedi company-quickhelp company markdown-mode open-junk-file anzu helm-swoop js2-mode fill-column-indicator quickrun py-yapf flycheck which-key bind-key smartparens rainbow-delimiters multiple-cursors migemo hungry-delete helm-c-yasnippet helm-ag expand-region)))
  '(recentf-auto-cleanup (quote never))
- '(server-auth-dir "~/.emacs.server" t))
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-mode)
+     (auto-complete-mode)
+     (outline-minor-mode . t))))
+ '(server-auth-dir "~/.emacs.server"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
