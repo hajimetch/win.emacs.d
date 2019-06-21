47a8e64f35ae5336647a842d863385eea0725fc1
Hajime ARAI <hajime.arai@gmail.com>
2019-06-21 15:42:42 +0900

    org-bulletsを導入。company-jediまわりの設定を改善

--- custom.el
+++ custom.el
@@ -9,12 +9,9 @@
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "713f898dd8c881c139b62cf05b7ac476d05735825d49006255c0a31f9a4f46ab" "4e63466756c7dbd78b49ce86f5f0954b92bf70b30c01c494b37c586639fa3f6f" default)))
  '(package-selected-packages
    (quote
-    (ini-mode use-package git-gutter helm-gtags yaml-mode php-mode web-mode shackle undo-tree helm-descbinds sticky flycheck-pos-tip helm-elscreen elscreen ssh-agency helm-projectile projectile magit ddskk json-mode rotate howm undohist recentf-ext volatile-highlights tangotango-theme company-jedi company-quickhelp company markdown-mode open-junk-file anzu helm-swoop js2-mode fill-column-indicator quickrun py-yapf flycheck which-key bind-key smartparens rainbow-delimiters multiple-cursors migemo hungry-delete helm-c-yasnippet helm-ag expand-region)))
+    (org-bullets exec-path-from-shell ini-mode use-package git-gutter helm-gtags yaml-mode php-mode web-mode shackle undo-tree helm-descbinds sticky flycheck-pos-tip helm-elscreen elscreen ssh-agency helm-projectile projectile magit ddskk json-mode rotate howm undohist recentf-ext volatile-highlights tangotango-theme company-jedi company-quickhelp company markdown-mode open-junk-file anzu helm-swoop js2-mode fill-column-indicator quickrun py-yapf flycheck which-key bind-key smartparens rainbow-delimiters multiple-cursors migemo hungry-delete helm-c-yasnippet helm-ag expand-region)))
  '(recentf-auto-cleanup (quote never))
- '(safe-local-variable-values
-   (quote
-    ((flycheck-mode)
-     (outline-minor-mode . t))))
+ '(safe-local-variable-values (quote ((flycheck-mode) (outline-minor-mode . t))))
  '(server-auth-dir "~/.emacs.server"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
